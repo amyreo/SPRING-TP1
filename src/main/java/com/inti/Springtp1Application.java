85b640e6787fad0046554f1cd83b621ef575ccab
theolorenzi <theo.lorenzi@laposte.net>
2022-11-04 14:16:36 +0100

    Premier commit pour initialiser tout le tralala

---
+++ src/main/java/com/inti/Springtp1Application.java
@@ -0,0 +1,13 @@
+package com.inti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtp1Application.class, args);
+	}
+
+}
